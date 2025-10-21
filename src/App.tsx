e143503c3f16d3c5c28e99294258031cbc918f9a
Anthony Wang <anthony.wang@shopify.com>
2025-10-21 02:49:51 -0400

    refactor: simplify App.tsx to use new SimpleResume component
    - Remove complex routing and multiple page components
    - Switch to single-page simple resume layout
    - Update imports to use new SimpleResume component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Experience from "./pages/Experience";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import "./App.css"; // Import app.css styles
-import NavBar from "./components/NavBar";
-import Landing from "./pages/Landing";
-import Footer from "./components/Footer";
-import Leadership from "./pages/Leadership";
-import ScrollToTop from "./components/ScrollToTop";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-
-// Home page component with all sections
-const HomePage = () => (
-  <>
-    <Landing/>
-    <About/>
-    <Experience/>
-    <Leadership/>
-    <Projects/>
-    <Footer/>
-    <ScrollToTop/>
-  </>
-);
+import SimpleResume from "./pages/SimpleResume";
+import "./SimpleResume.css";
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <NavBar/>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/" element={<SimpleResume />} />
       </Routes>
     </Router>
   );
